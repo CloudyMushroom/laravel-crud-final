0eb3d2b25c7acdc66f89faafdf5a048bc6b0f4be
Lejexy <jdejesus965@gmail.com>
2024-12-27 17:19:06 +0800

    customizing tailwindcss

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -7,45 +7,49 @@
     <title>Document</title>
     @vite('resources/css/app.css')
 </head>
-<body> 
+<body class="body"> 
     <!-- FOR READ in CRUD -->
 
-    <h1 class="text-red-300">landing page</h1>
-    <form action="{{route('student.create')}}" method="GET">
-        <button>CREATE</button>
-    </form>
     
-    <br>
-    <br>
-    <br>
-    <table border="2">
-        <tr>
-            <th>Learners Reference Number</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Learners Age</th>
-       
-        </tr>
-
-        @foreach ($students as $student)
-        <tr>
-            <td>{{$student->LearnersNumber}}</td>
-            <td>{{$student->FirstName}}</td>
-            <td>{{$student->LastName}}</td>
-            <td>{{$student->LearnersAge}}</td>
-            <td><a href="{{route('student.edit', $student->id)}}">Edit</a></td>
-            <td>
-                <form action="{{route('student.delete', $student->id)}}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit">DELETE</button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
-      
-      
-    </table>
+    <div class="main-content">
+        <h1 class="title">LANDING PAGE</h1>
+        <form class="form-btn"action="{{route('student.create')}}" method="GET">
+            <button class="btn-create">CREATE</button>
+        </form>
+        
+        <br>
+        <br>
+        <br>
+        <table border="2">
+            <tr>
+                <th>Learners Reference Number</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Learners Age</th>
+           
+            </tr>
+    
+            @foreach ($students as $student)
+            <tr>
+                <td>{{$student->LearnersNumber}}</td>
+                <td>{{$student->FirstName}}</td>
+                <td>{{$student->LastName}}</td>
+                <td>{{$student->LearnersAge}}</td>
+                <td><a href="{{route('student.edit', $student->id)}}">Edit</a></td>
+                <td>
+                    <form action="{{route('student.delete', $student->id)}}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn-del" type="submit">Delete</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+          
+          
+        </table>
+    </div>
+    
     
 </body>
 </html>
